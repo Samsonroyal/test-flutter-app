d5a0cb6cbc79f7720204005174582101138fa86e
Samsonroyal <werefast2000@gmail.com>
2024-01-10 18:38:03 +0000

    fix imports in test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:test_app_one/main.dart';
+import 'package:test_app_one/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
